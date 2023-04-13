15efdc963bcdef4b21a53c655325952bc1d59dfc
Joash Dellota <joash.dellota@novare.com.hk>
2023-04-13 16:17:16 +0800

    Initial commit

---
+++ app/src/main/java/ph/stacktrek/novare/ecommerce/dellota/joash/LoginActivity.kt
@@ -0,0 +1,97 @@
+package ph.stacktrek.novare.ecommerce.dellota.joash
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.activity.result.contract.ActivityResultContracts
+import com.google.android.material.snackbar.Snackbar
+import ph.stacktrek.novare.ecommerce.dellota.joash.databinding.ActivityLoginBinding
+import ph.stacktrek.novare.ecommerce.dellota.joash.utility.PreferenceUtility
+
+class LoginActivity : AppCompatActivity() {
+
+    private lateinit var register_uname:String
+    private lateinit var register_pass:String
+    private lateinit var binding: ActivityLoginBinding
+
+    private val launchRegister = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    )
+    { result ->
+
+        val data = result.data
+        register_uname = data?.getStringExtra("register_username").toString()
+        register_pass = data?.getStringExtra("register_password").toString()
+
+        binding.usernametext.setText(register_uname)
+        binding.passwordtext.setText(register_pass)
+
+        Snackbar.make(binding.root,
+            "Registered ${data!!.getStringExtra("register_username")}",
+            Snackbar.LENGTH_LONG).show()
+
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.registerButton.setOnClickListener {
+            val goToRegister = Intent(
+                applicationContext,
+                RegisterActivity::class.java
+            )
+
+            launchRegister.launch(goToRegister)
+        }
+
+        binding.loginButton.setOnClickListener {
+
+            var username = binding.usernametext.text.toString()
+            var password = binding.passwordtext.text.toString()
+
+            if(username == "admin" && password == "admin") {
+                val goToMain = Intent(
+                    applicationContext,
+                    MainActivity::class.java
+                )
+                goToMain.putExtra("username", username)
+
+                val bundle = Bundle()
+                bundle.putString("bundle_username", username)
+                goToMain.putExtras(bundle)
+
+                //Dito nakalagay ayung Share preferences
+
+                PreferenceUtility(applicationContext).apply {
+                    saveStringPreferences("username", binding.usernametext.text.toString())
+                    saveStringPreferences("password", binding.passwordtext.text.toString())
+                }
+
+                startActivity(goToMain)
+                finish()
+            }else{
+                Snackbar.make(binding.root,
+                    "Please provide admin/admin",
+                    Snackbar.LENGTH_SHORT).show()
+            }
+        }
+        //Dito nakalagay ayung Share preferences
+        PreferenceUtility(applicationContext).apply {
+            saveStringPreferences("username", binding.usernametext.text.toString())
+            saveStringPreferences("password", binding.passwordtext.text.toString())
+        }
+    }
+    override fun onBackPressed() {
+        super.onBackPressed()
+        val goToMain = Intent(applicationContext,
+            LoginActivity::class.java)
+
+        startActivity(goToMain)
+        finish()
+
+    }
+
+}
---
+++ app/src/main/java/ph/stacktrek/novare/ecommerce/dellota/joash/MainActivity.kt
@@ -0,0 +1,101 @@
+package ph.stacktrek.novare.ecommerce.dellota.joash
+
+import android.app.Dialog
+import android.content.DialogInterface
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AlertDialog
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.ItemTouchHelper
+import androidx.recyclerview.widget.LinearLayoutManager
+import ph.stacktrek.novare.ecommerce.dellota.joash.adapters.ProductAdapter
+import ph.stacktrek.novare.ecommerce.dellota.joash.adapters.SwipeCallback
+import ph.stacktrek.novare.ecommerce.dellota.joash.dao.ProductDAO
+import ph.stacktrek.novare.ecommerce.dellota.joash.dao.ProductDAOSQLLiteImplementation
+import ph.stacktrek.novare.ecommerce.dellota.joash.dao.ProductDAOStubImplementation
+import ph.stacktrek.novare.ecommerce.dellota.joash.databinding.ActivityMainBinding
+import ph.stacktrek.novare.ecommerce.dellota.joash.databinding.DialogueAddProductBinding
+import ph.stacktrek.novare.ecommerce.dellota.joash.model.Product
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var productAdapter:ProductAdapter
+    private lateinit var productDAO: ProductDAO
+    private lateinit var itemTouchHelper: ItemTouchHelper
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val bundle = intent.extras
+        Log.d("MAIN ACTIVITY","USERNAME BUNDLE: ${bundle!!.getString("bundle_username")}")
+
+        val extra = intent.getStringExtra("username")
+        Log.d("MAIN ACTIVITY","USERNAME EXTRA : $extra")
+
+        loadProducts()
+
+        binding.fabAddProductButton.setOnClickListener{
+            showAddProductDialogue().show()
+        }
+
+    }
+
+    fun loadProducts(){
+//        productDAO = ProductDAOStubImplementation()
+        val productDAO = ProductDAOSQLLiteImplementation(applicationContext)
+        productAdapter = ProductAdapter(applicationContext,
+            productDAO.getProducts())
+        with(binding.productsList){
+//            layoutManager = LinearLayoutManager(applicationContext,
+//                LinearLayoutManager.HORIZONTAL,
+//                false)
+            layoutManager = GridLayoutManager(applicationContext,2)
+            adapter = productAdapter
+        }
+        var swipeCallback = SwipeCallback(0, ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT)
+        swipeCallback.productAdapter = productAdapter
+        itemTouchHelper = ItemTouchHelper(swipeCallback).apply {
+            attachToRecyclerView(binding.productsList)
+        }
+    }
+
+    fun showAddProductDialogue():Dialog{
+        return this!!.let{
+            val builder = android.app.AlertDialog.Builder(it)
+            var dialogueAddProductBinding : DialogueAddProductBinding=
+                DialogueAddProductBinding.inflate(it.layoutInflater)
+            with(builder){
+                setPositiveButton("ADD",DialogInterface.OnClickListener{dialog,id ->
+                    val product = Product("")
+                    product.name = dialogueAddProductBinding.productName.text.toString()
+
+//                    val productDAO = ProductDAOStubImplementation()
+                    val productDAO = ProductDAOSQLLiteImplementation(applicationContext)
+                    productDAO.addProduct(product)
+                    productAdapter.addProduct(product)
+                })
+                setNegativeButton("CANCEL",DialogInterface.OnClickListener{dialog,id ->
+
+                })
+                setView(dialogueAddProductBinding.root)
+                create()
+            }
+        }
+    }
+    override fun onBackPressed() {
+        super.onBackPressed()
+        val goToMain = Intent(applicationContext,
+            LoginActivity::class.java)
+
+        startActivity(goToMain)
+        finish()
+
+    }
+}
